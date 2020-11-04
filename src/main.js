bacedb709f0792ef99b12ed706cf626933326860
DianyelaMaldonado <dianyela.y.maldonado@gmail.com>
2020-11-04 17:18:34 -0600

    Added som changes in slider implementation

--- src/main.js
+++ src/main.js
@@ -5,8 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
         edge: 'right',
     };
     var instances = M.Sidenav.init(elems, options);
+    slickSlider();
 });
 
 
 
+  
+
+function slickSlider() {
+    $('.cs__slider-wrapper').slick(
+      {
+        dots: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        autoplay:true,
+        adaptiveHeight:true,
+        autoplaySpeed:3000,
+      }
+    );
+}
 
